c7507bc9be1b011ab7a32f17c74c92256136ef1f
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-03 17:48:33 +0400

    refactor: Refactor userController.js for improved readability and error handling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,44 +8,38 @@ const generateUniqueCode = () => {
 };
 
 export const createUser = async (req, res) => {
-    const { email, password, firstName, lastName } = req.body;
-    const { file } = req;
-  
-    if(!email || !password || !firstName || !lastName) {
-        console.log("email", email)
-        console.log("password", password)
-        console.log("firstName", firstName)
-        console.log("lastName", lastName)
-        console.log("file", file)
-      return res.status(400).json({ message: "All fields are required" });
+  const { email, password, firstName, lastName } = req.body;
+  const { file } = req;
+
+  if (!email || !password || !firstName || !lastName) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      return res.status(400).json({ message: "ელ-ფოსტა გამოყენებულია." });
     }
-    console.log("password", file)
-    try {
-      const userExists = await User.findOne({ where: { email } });
-      if (userExists) {
-        return res.status(400).json({ message: "ელ-ფოსტა გამოყენებულია." });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const verificationCode = generateUniqueCode();
-  
-      const newUser = new User({
-        email,
-        password: hashedPassword,
-        firstName,
-        lastName,
-        verificationCode,
-        profilePicture: "profilePictures/" + file.originalname,
-      });
-  
-      await newUser.save();
-  
-      await sendVerificationEmail(email, verificationCode);
-  
-      res.status(201).json({ message: "მომხმარებელი შეიქმნა წარმატებით" });
-    } catch (error) {
-      console.error("Error registering user:", error);
-      res.status(500).json({ message: "Server error" });
-    }
-  };
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const verificationCode = generateUniqueCode();
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      verificationCode,
+      profilePicture: "profilePictures/" + file.originalname,
+    });
+
+    await newUser.save();
+
+    await sendVerificationEmail(email, verificationCode);
+
+    res.status(201).json({ message: "მომხმარებელი შეიქმნა წარმატებით" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
