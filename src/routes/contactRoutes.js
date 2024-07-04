f069c31b9cc204bcd79a90a61c967614fe17564c
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-04 12:38:27 +0400

    feat: Add route for requesting to add a category

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -26,4 +26,27 @@ router.post("/contact", async (req, res) => {
   }
 });
 
+router.post("/request_to_add_category", async (req, res) => {
+  const { categoryName, email, comment } = req.body;
+
+  if (!categoryName) {
+    return res.status(400).json({ message: "Category name is required" });
+  }
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_TO,
+    subject: `ახალი კატეგორიის დამატების მოთხოვნა`,
+    text: `კატეგორიის სახელი: ${categoryName}\nელ-ფოსტა: ${email}\nკომენტარი: ${comment}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Message sent" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ message: "Error sending email" });
+  }
+});
+
 export default router;
